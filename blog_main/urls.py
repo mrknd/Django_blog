563256dbd21c05a0ed264ed631adc9451b8c74b6
mrkn <markiian.design@gmail.com>
2024-05-20 11:51:19 +0200

    added category CRUD funcionality

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -36,6 +36,7 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
+    path('dashboard/', include('dashboards.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog_main/views.py
+++ blog_main/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import auth
